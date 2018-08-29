be3489e4333122f3e40251abac47b400e6048507
Kipopay <kipodeveloper@gmail.com>
2018-08-29 11:44:52 +0430

    Change description of function

--- kipo-woocommerce.php
+++ kipo-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: افزونه درگاه پرداخت کیپو برای ووکامرس
  * Plugin URI: kipopay.com
  * Description: افزودن درگاه پرداخت کیپو
- * Version: 0.5.2
+ * Version: 0.5.2.1
  * Author: Kipo Development team
  * License: GPL2
  * Domain Path: /languages/
